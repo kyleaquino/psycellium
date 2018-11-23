208daf0ab5c915f96de10869286a648eb11f0b60
Kyle Aquino <kshaunaquino@gmail.com>
2018-11-23 21:01:46 +0800

    working files

--- Psycellium/settings.py
+++ Psycellium/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = (BASE_DIR)
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +58,9 @@ ROOT_URLCONF = 'Psycellium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'app/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- ethereum/contracts/members/MemberFactory.sol
+++ ethereum/contracts/members/MemberFactory.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
-import "Members.sol";
+import "./Members.sol";
 
 contract MemberFactory {
 
