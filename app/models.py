88457db43ba3aa1ded939e1a7c56078138f2afb9
David Boado <34389794+henouji@users.noreply.github.com>
2018-11-24 18:43:01 +0800

    Centralized Database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User as auth_user
 
-# Create your models here.
+class Cooperative(models.Model):
+    name = models.CharField(max_length=25,)
+
+    def __str__(self):
+        return str(self.name)
+
+class Profile(models.Model):
+    user = models.OneToOneField(auth_user, on_delete=models.CASCADE,);
+    eth_address = models.CharField(max_length=255,)
+    coop = models.ForeignKey(Cooperative, null=True, on_delete=models.CASCADE)
+    private_key = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return str(self.user)
