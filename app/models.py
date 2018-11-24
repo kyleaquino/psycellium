c864f71cfa550bed4bf07c4033806ad9398739a0
David Boado <34389794+henouji@users.noreply.github.com>
2018-11-25 06:52:48 +0800

    Model changes

--- app/models.py
+++ app/models.py
@@ -3,15 +3,33 @@ from django.contrib.auth.models import User as auth_user
 
 class Cooperative(models.Model):
     name = models.CharField(max_length=25,)
+    block_id = models.IntegerField(max_lenght=25, null=False)
+    block_address = model.CharField(max_lenght=255, null=False)
+    description = model.CharField(max_lenght=50, null=True)
+    issueDate = model.DateField(null=False)
 
     def __str__(self):
         return str(self.name)
 
 class Profile(models.Model):
-    user = models.OneToOneField(auth_user, on_delete=models.CASCADE,);
+    # ROLE_TYPES = (
+    #     ('Member'),
+    #     ('Staff'),
+    #     ('Director'),
+    #     ('Staff'),
+    #     ('President'),
+    #     ('Manager'),
+    #     ('Secretary'),
+    #     ('Auditor'),
+    #     ('Treasurer')
+    # )
+    user = models.OneToOneField(auth_user, on_delete=models.CASCADE,)
     eth_address = models.CharField(max_length=255,)
+    block_id = models.IntegerField(max_lenght=25, null=False)
+    block_address = model.CharField(max_lenght=255, null=False)
     coop = models.ForeignKey(Cooperative, null=True, on_delete=models.CASCADE)
     private_key = models.CharField(max_length=255, null=True)
+    # role = models.CharField(max_length=15,choices=ROLE_TYPES, default='Member')
 
     def __str__(self):
         return str(self.user)
