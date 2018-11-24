a98d1bed948ab521aac1d07301dcc1e12d9d6636
Kyle Aquino <kshaunaquino@gmail.com>
2018-11-25 01:48:06 +0800

    deleted unneccessary solidity files

--- ethereum/sample-contracts/WETH.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-
-contract WETH {
-    mapping (address => uint) public balanceOf;
-    function deposit() public payable;
-    function withdraw(uint wad) public;
-    function approve(address guy, uint wad) public returns (bool);
-}
