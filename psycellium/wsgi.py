4680214d41c82ef02e60726f94f817314e80b4c0
Kyle Aquino <kshaunaquino@gmail.com>
2018-11-24 13:14:30 +0800

    updates

--- psycellium/wsgi.py
+++ psycellium/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Psycellium project.
+WSGI config for psycellium project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Psycellium.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psycellium.settings")
 
 application = get_wsgi_application()
