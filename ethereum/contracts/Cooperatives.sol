6a3c5ce2670c287482072459a5fef9a869795bfe
Kyle Aquino <kshaunaquino@gmail.com>
2019-01-18 22:26:32 +0800

    update

--- ethereum/contracts/Cooperatives.sol
+++ ethereum/contracts/Cooperatives.sol
@@ -8,119 +8,193 @@ contract Cooperatives {
   event DirectorRemoval(address indexed account);
   event Deposit(address indexed sender, uint amount);
   event Withdraw(address indexed sender, uint amount);
+  event Confirmation(address indexed sender, uint indexed transactionId);
+  event Revocation(address indexed sender, uint indexed transactionId);
+  event Submission(uint indexed transactionId);
+  event Execution(uint indexed transactionId);
+  event ExecutionFailure(uint indexed transactionId);
+  event RequirementChange(uint required);
 
   /* PUBLIC DATA */
   string public name;
   string public description;
   address[] public members;
   address[] public directors;
-  uint public totalBalance;
   uint public transactionCount;
+  uint public required;
   uint256 public defaultJoinFee = 200000000000000000; // 25 USD
 
   mapping (uint => Transaction) public transactions;
+  mapping (uint => mapping (address => bool)) public confirmations;
   mapping (address => bool) public isDirector;
   mapping (address => bool) public isMember;
   mapping (address => uint) public balance;
 
-  /* MODIFIERS */
-  modifier directorExists(address director) {
-    if (!isDirector[director])
-      throw;
-    _;
-  }
-
-  modifier memberExists(address member) {
-    if (!isMember[member])
-      throw;
-    _;
-  }
-
-  modifier notNull(address _address) {
-    if (_address == 0)
-      throw;
-    _;
+  struct Transaction {
+    address destination;
+    uint value;
+    bytes data;
+    bool executed;
   }
 
   /* Sets the contract deployer as the director and a member temporarily */
-  constructor(bytes32 _name, bytes32 _desc)
-  public
-  {
+  constructor(string _name, string _desc) public {
     name = _name;
     description = _desc;
+    required = 1;
     directors.push(msg.sender);
     isDirector[msg.sender] = true;
-    DirectorRegistered(msg.sender)
+    emit DirectorRegistered(msg.sender);
     members.push(msg.sender);
     isMember[msg.sender] = true;
-    MemberRegistered(msg.sender);
+    emit MemberRegistered(msg.sender);
   }
 
-  function joinCoop() payable
-  public
-  {
-    require(msg.value == defaultJoinFee);
-    members.push(msg.sender)
+  function addTransaction(address destination, uint value, bytes data)
+  internal returns (uint transactionId) {
+    require(destination != 0);
+    transactionId = transactionCount;
+    transactions[transactionId] = Transaction({
+      destination: destination,
+      value: value,
+      data: data,
+      executed: false
+    });
+    transactionCount += 1;
+    emit Submission(transactionId);
+  }
+
+  function joinCoop() payable public {
+    require(
+      !isMember[msg.sender] &&
+      msg.value == defaultJoinFee
+    );
+    members.push(msg.sender);
     isMember[msg.sender] = true;
-    MemberRegistered(msg.sender)
+    emit MemberRegistered(msg.sender);
   }
 
-  function setDirector(address member)
-  public
-  directorExists(msg.sender)
-  memberExists(member)
-  {
-    directors.push(member)
+  function setDirector(address member) public {
+    require(
+      isMember[member] &&
+      isDirector[msg.sender]
+    );
+    directors.push(member);
     isDirector[member] = true;
-    DirectorRegistered(member)
+    emit DirectorRegistered(member);
   }
 
-  function deposit(uint amount) payable
-  public
-  memberExists(msg.sender)
-  {
-    require(msg.value == amount);
+  function deposit(uint amount) payable public {
+    require(
+      msg.value == amount &&
+      isMember[msg.sender]
+    );
     balance[msg.sender] += amount;
-    totalBalance += amount
-    Deposit(msg.sender, amount)
+    balance[address(this)] += amount;
+    emit Deposit(msg.sender, amount);
   }
 
-  function withdraw(uint amount)
-  public
-  memberExists(msg.sender)
-  {
-    require(msg.value == amount);
-    require(balance[msg.sender] >= amount);
-    require(totalBalance >= amount);
+  function withdraw(uint amount) public {
+    require(
+      isMember[msg.sender] &&
+      balance[address(this)] >= amount &&
+      balance[msg.sender]>=amount
+    );
     msg.sender.transfer(amount);
-    balance[msg.sender] -= amount
-    totalBalance -= amount
-    Withdraw(msg.sender, amount)
+    balance[msg.sender] -= amount;
+    balance[address(this)] -= amount;
+    emit Withdraw(msg.sender, amount);
+  }
+
+  function isConfirmed(uint transactionId)
+  public constant returns (bool) {
+    uint count = 0;
+    for (uint i=0; i<directors.length; i++) {
+      if (confirmations[transactionId][directors[i]])
+        count += 1;
+      if (count == required)
+        return true;
+    }
+  }
+
+  function sumbitRequest(address destination, uint value, bytes data)
+  public returns (uint transactionId){
+    require(isMember[msg.sender]);
+    transactionId = addTransaction(destination, value, data);
+    confirmTransaction(transactionId);
+  }
+
+  function confirmTransaction(uint transactionId) public {
+    require(
+      isDirector[msg.sender] &&
+      transactions[transactionId].destination != 0 &&
+      !confirmations[transactionId][msg.sender]
+    );
+    confirmations[transactionId][msg.sender] = true;
+    emit Confirmation(msg.sender, transactionId);
+    executeTransaction(transactionId);
+  }
+
+  function revokeTransaction(uint transactionId) public {
+    require(
+      isDirector[msg.sender] &&
+      confirmations[transactionId][msg.sender] &&
+      !transactions[transactionId].executed
+    );
+    confirmations[transactionId][msg.sender] = false;
+    emit Revocation(msg.sender, transactionId);
+  }
+
+  function executeTransaction(uint transactionId) public {
+    require(
+      !transactions[transactionId].executed
+    );
+    if (isConfirmed(transactionId)) {
+      Transaction storage txn = transactions[transactionId];
+      txn.executed = true;
+      if (txn.destination.call.value(txn.value)(txn.data))
+        emit Execution(transactionId);
+      else {
+        emit ExecutionFailure(transactionId);
+        txn.executed = false;
+      }
+    }
   }
 
   /* View Functions */
   function getCoopName()
-  public view returns (string)
-  {
+  public view returns (string) {
     return name;
   }
 
   function getCoopMembers()
-  public view returns (address[])
-  {
+  public view returns (address[]) {
     return members;
   }
 
   function getCoopDescription()
-  public view returns (string)
-  {
+  public view returns (string) {
     return description;
   }
 
   function getCoopBalance()
-  public view returns (uint)
-  {
-    return totalBalance;
+  public view returns (uint) {
+    return balance[address(this)];
+  }
+
+  function getConfirmationCount(uint transactionId)
+  public view returns (uint count) {
+    for (uint i=0; i<directors.length; i++)
+      if (confirmations[transactionId][directors[i]])
+        count += 1;
+  }
+
+  function getTransactionCount(bool pending, bool executed)
+  public view returns (uint count) {
+    for (uint i=0; i<transactionCount; i++)
+      if (pending && !transactions[i].executed
+          || executed && transactions[i].executed)
+          count += 1;
   }
 
 }
